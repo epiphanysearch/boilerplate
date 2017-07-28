51156db2f931f84ca7d9eea7f626e7ff47cfb3bd
Thomas Nadin <nadinengland@gmail.com>
2017-07-28 16:09:27 +0100

    Changing default JS to represent current workflow

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,11 +1,25 @@
-define(["jquery"], function ($) {
-    $(function () {
-        var main = {
-            init: function () {
-                // Your code here
-            }
-        };
+import $ from 'jquery';
 
-        main.init();
-    });
-});
+const Main = class {
+    constructor(el) {
+        this.el = $(el).get(0);
+        this.$el = $(this.el);
+
+        this._init_ui();
+        this._init_events();
+    }
+
+    _init_ui() {
+        this.$header = this.$el.find('header');
+    }
+
+    _init_events() {
+        this.$el.on('click', 'h1, h2, h3, h4, h5, h6', this.on_element_click.bind(this));
+    }
+
+    on_element_click(e) {
+        $(e.currentTarget).css('color', 'red');
+    }
+};
+
+export default Main;
